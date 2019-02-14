314a32e857c753b7ec38acc6e915b911d2db1f1a
visualstudio <visualstudio@230-PC12>
2019-02-14 09:00:11 -0500

    //c0740950

--- c0740950_6/js/main.js
+++ c0740950_6/js/main.js
@@ -1,1 +1,10 @@
-﻿// Your code here!
+﻿// Your code here
+
+    function addNumbers(j) {
+            var sum = 0;
+            for (i = 0; i < j; i++) {
+        sum += i;
+
+            }
+    return sum;
+}
